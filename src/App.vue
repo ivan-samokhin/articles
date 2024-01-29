9751a88aa2fffa62cedfe73fec3e0a363420bc63
Ivan Samokhin <ivan@exton.is>
2024-01-29 03:35:00 +0000

    initial-deploy

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import CuttingEdge from './views/CuttingEdge.vue'
 
 </script>
 
 <template>
  <RouterView></RouterView>
+ <CuttingEdge/>
 </template>
 
 <style scoped>
